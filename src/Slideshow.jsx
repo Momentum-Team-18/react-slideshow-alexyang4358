c0c809cde2d3dfd2316aa85ded6a7a2d2fd07d5b
alexyang4358 <alexyang4358@gmail.com>
2023-06-12 11:52:19 -0400

    basic project

--- src/Slideshow.jsx
+++ src/Slideshow.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import filmData from './film-data.json'
 import {useState} from 'react'
 
-function Slideshow(props) {
+
+function Slideshow() {
+
     const [currentSlide, setCurrentSlide] = useState(0)
-    console.log(currentSlide.id)
+    console.log(filmData.length)
 
     const handleStartOver = () => {
         setCurrentSlide(0)
@@ -14,18 +16,24 @@ function Slideshow(props) {
         setCurrentSlide(currentSlide + 1)
     }
 
+    const handlePreviousSlide = () => {
+        setCurrentSlide(currentSlide - 1)
+    }
+
     return (
         <>
-        <section className='slide'> 
+        <section className='slide'>
             <img className='image' src={filmData[currentSlide].image}></img>
             <div className='content'>
                 <h1 className='title'>{filmData[currentSlide].title}</h1>
                 <p className='title'>{filmData[currentSlide].original_title}</p>
                 <p className='paragraph'>{filmData[currentSlide].release_date}</p>
                 <p className='paragraph'>{filmData[currentSlide].description}</p>
-                <button onClick = {handleStartOver} className='button'>START OVER</button>
-                <button className='button'>BACK</button>
-                <button onClick = {handleNextSlide}className='button'>NEXT</button>
+            <div className='content'>
+                <button onClick = {handleStartOver} disabled={currentSlide === 0} className='button'>START OVER</button>
+                <button onClick = {handlePreviousSlide} disabled={currentSlide === 0} className='button'>BACK</button>
+                <button onClick = {handleNextSlide} disabled={currentSlide === filmData.length - 1} className='button'>NEXT</button>
+                </div>
             </div>
         </section>
         </>
